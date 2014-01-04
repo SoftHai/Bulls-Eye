3d3740a851240a2033abf1e1b4576d5d69a4c77e
SoftHai <SoftHai@gmx.de>
2014-01-04 18:15:03 +0100

    Fix drone.io build

--- lib/BullsEyeServer/bulls_eye_server.dart
+++ lib/BullsEyeServer/bulls_eye_server.dart
@@ -3,7 +3,7 @@ library softhai.bulls_eye.Server;
 import 'dart:io';
 import '../BullsEyeCommon/bulls_eye_common.dart' as common;
 
-part 'src/server.dart';
+part 'src/Server.dart';
 part 'src/Routing/Route.dart';
 part 'src/Routing/LogicRoute.dart';
 part 'src/Routing/FileRoute.dart';
